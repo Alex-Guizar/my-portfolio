f217fc1c521fa1a0b8758675b9881cdce6ef9457
Alex Guizar <alexguizar90@gmail.com>
2022-01-25 21:20:58 -0800

    Imported and implemented layout components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// Packages
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// Material Components
+import Container from '@mui/material/Container';
+
+// Custom Components
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Greeting from './components/Greeting';
+import AboutMe from './components/AboutMe';
+import MyWork from './components/MyWork';
+import ContactMe from './components/ContactMe';
+import Footer from './components/Footer';
+
+// CSS
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ResponsiveAppBar />
+
+      <Container maxWidth="lg">
+        <Greeting />
+
+        <AboutMe />
+
+        <MyWork />
+
+        <ContactMe />
+
+        <Footer />
+      </Container>
+    </React.Fragment>
   );
 }
 
