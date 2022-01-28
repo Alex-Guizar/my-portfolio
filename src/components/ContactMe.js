34a631991f5f8a9d7d555cc9d74660bde1c48768
Alex Guizar <alexguizar90@gmail.com>
2022-01-27 22:41:18 -0800

    Added content to contact me component

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -3,10 +3,21 @@ import React from 'react';
 
 // Material Components
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const ContactMe = () => {
   return (
-    <Box>Contact Me</Box>
+    <Box marginTop={8}>
+      <Typography
+        variant="h2"
+        container="h2"
+      >Contact Me</Typography>
+
+      <Typography marginTop={4} paragraph>Have a question or want to collaborate on a project? Feel free to email me.</Typography>
+
+      <Button variant="contained" href="mailto:alexguizar90@gmail.com">Contact Me</Button>
+    </Box>
   );
 }
 
