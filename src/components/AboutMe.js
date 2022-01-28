bd575154b871fea924fc9e39b015047ffb20b7b9
Alex Guizar <alexguizar90@gmail.com>
2022-01-27 22:13:16 -0800

    Added about me information and portrait

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,16 +7,49 @@ import Typography from '@mui/material/Typography';
 
 const AboutMe = () => {
   return (
-    <Box>
+    <Box sx={{ marginTop: 8 }}>
       <Typography
         variant="h2"
         container="h2"
       >About Me</Typography>
 
-      <Box>
-        {/** My image goes here */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          marginTop: 4
+        }}
+      >
+        <Box
+          sx={{
+            textAlign: 'center',
+            paddingRight: { md: 2 }
+          }}
+        >
+          <img
+            src="images/AlexCropBW.jpg"
+            alt="Image of me in an elevator"
+            style={{ 
+              width: '100%',
+              maxWidth: 350, 
+              borderRadius: 50,
+            }}
+          />
+        </Box>
 
-        <Typography paragraph>lorem</Typography>
+        <Box
+          sx={{
+            flexGrow: 1,
+            flexShrink: 1,
+            marginTop: { xs: 2, md: 0 }
+          }}
+        >
+          <Typography paragraph>Hello, my name is Alex Guizar and I am a Full-Stack Developer with a strong background in Front-End Development. My goal is to create clean, snappy web applications with carefully crafted code that give the user an enjoyable experience.</Typography>
+
+          <Typography paragraph>Since entering the web development field in 2013, I have pushed myself to continue learning and expand my toolkit.</Typography>
+
+          <Typography paragraph>When not online, I can be found traveling the world, exploring new culinary adventures both in the kitchen and abroad, snowboarding in the winter, or running raids in Final Fantasy XIV where I handle callouts for my team and help devise new strategies.</Typography>
+        </Box>
       </Box>
     </Box>
   );
