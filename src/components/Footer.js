79c1f4a7065403404fe3d820a7809f5251268ef0
Alex Guizar <alexguizar90@gmail.com>
2022-01-27 22:40:44 -0800

    Added email, social links and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,32 @@ import React from 'react';
 
 // Material Components
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
   return (
-    <Box>Footer</Box>
+    <Box marginTop={8}>
+      <Typography paragraph>Email: <Link href="mailto:alexguizar90@gmail.com">alexguizar90@gmail.com</Link></Typography>
+
+      <Box>
+        <IconButton aria-label="Visit my LinkedIn" href="https://www.linkedin.com/in/alexander-guizar-32649263/" target="_blank" rel="noreferrer">
+          <LinkedInIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton aria-label="Visit my GitHub" href="https://github.com/Alex-Guizar" target="_blank" rel="noreferrer">
+          <GitHubIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton aria-label="Visit my Twitter" href="https://twitter.com/alexguizar90" target="_blank" rel="noreferrer">
+          <TwitterIcon fontSize="large" />
+        </IconButton>
+      </Box>
+    </Box>
   );
 }
 
