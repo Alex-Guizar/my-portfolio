56b0b89c5a51eee71fc52f4b206e89ccafeb6d9c
Alex Guizar <alexguizar90@gmail.com>
2022-02-07 19:29:30 -0800

    Removed unused social media from the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,15 +9,12 @@ import Link from '@mui/material/Link';
 import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
   return (
     <Container maxWidth="lg">
       <Box marginTop={8} paddingBottom={8} textAlign="center">
         <Typography paragraph>Email: <Link href="mailto:alexguizar90@gmail.com">alexguizar90@gmail.com</Link></Typography>
-        
-        <Typography paragraph>Medium: <Link href="https://medium.com/@alexguizar90">https://medium.com/@alexguizar90</Link></Typography>
 
         <Box>
           <IconButton aria-label="Visit my LinkedIn" href="https://www.linkedin.com/in/alexander-guizar-32649263/" target="_blank" rel="noreferrer">
@@ -26,10 +23,6 @@ const Footer = () => {
 
           <IconButton aria-label="Visit my GitHub" href="https://github.com/Alex-Guizar" target="_blank" rel="noreferrer">
             <GitHubIcon fontSize="large" />
-          </IconButton>
-
-          <IconButton aria-label="Visit my Twitter" href="https://twitter.com/alexguizar90" target="_blank" rel="noreferrer">
-            <TwitterIcon fontSize="large" />
           </IconButton>
         </Box>
       </Box>
